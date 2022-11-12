fc010bcf3931242a44d28a793164ffa9fa5f236f
Bryan Rodriguez <b.ra@live.com.mx>
2022-11-12 11:11:20 -0600

    maquetando fred

---
+++ fredFabulous/main.js
@@ -0,0 +1,20 @@
+const b1 = document.getElementById("b1");
+const b2 = document.getElementById("b2");
+const b3 = document.getElementById("b3");
+const b4 = document.getElementById("b4");
+const b5 = document.getElementById("b5");
+const b6 = document.getElementById("b6");
+const b7 = document.getElementById("b7");
+const b8 = document.getElementById("b8");
+const b9 = document.getElementById("b9");
+const b10 = document.getElementById("b10");
+const b11 = document.getElementById("b11");
+const b12 = document.getElementById("b12");
+
+const random = () => Math.floor(Math.random() * 12);
+
+const iluminado = [];
+
+const iluminar = () => {
+  iluminado.push(random());
+};
